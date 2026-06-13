390cc740ea18570d89e1024343c12bd857f4b2ff
Pro-Gitttt <tarek.gadhgadhi@esprit.tn>
2026-06-13 01:58:54 +0100

    fix: use relative prometheus URL to avoid CORS issues in production build

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -5,6 +5,6 @@ export const environment = {
   apiSecurity: '/api/security',
   apiPipeline: '/api/pipeline',
   apiNotification: '/api/notifications',
-  apiPrometheus: 'http://192.168.56.20:32090',
+  apiPrometheus: '/prometheus',
   apiAudit: '/api/audit',
 };
